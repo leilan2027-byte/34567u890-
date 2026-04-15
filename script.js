106ef5bbcfa43a590f4fb7f34a9a953576aab0d2
leilan2027-byte <leilan2027@gmail.com>
2026-04-15 17:46:08 +0000

    3234567

--- script.js
+++ script.js
@@ -25,28 +25,36 @@ function processForm(event) {
         birthdate: document.getElementById("birthdate").value,
         favColor: document.getElementById("favcolor").value
     }
+
     console.log(newUser);
     // TODO: Call the addUser function and pass the newUser object as a parameter
+    addUser(newUser);
+
 }
+
 
 function addUser(user) {
     // Create a div to hold the user's info
     let div = document.createElement("div");
     // TODO: Set the background color of the div to the user's favorite color
+    div.style.backgroundColor = user.favColor;
 
     // Create a heading for the user's name
     let name = document.createElement("h2");
     // TODO: Set the text using the object data
+    name.innerText = user.name;
     div.appendChild(name); // Add the heading to the div
 
     // Create a paragraph for the user's email
     let email = document.createElement("p");
     // TODO: Set the text using the object data
+    email.innerText = user.email;
     div.appendChild(email); // Add the paragraph to the div
     
     // Create a paragrapn for the user's birthdate
     let birthdate = document.createElement("p");
     // TODO: Set the text using the object data
+    birthdate.innerText = user.birthdate
     div.appendChild(birthdate); // Add the paragraph to the div
 
     document.body.appendChild(div); // Add the div to the page
